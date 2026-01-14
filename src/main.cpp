e7a9ad226844587fc97ac5435f3498c2582af6ad
Buay Dak <buaypd@gmail.com>
2026-01-14 18:15:13 +0000

    Changed the backdrop to skyblue.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_core.h>
+
+
 int main()
 {
     bn::core::init();
-    bn::backdrop::set_color(bn::color(20, 20, 31));
-
+    //bn::backdrop::set_color(bn::color(20, 20, 31)); // pld color
+    //bn::backdrop::set_color(bn::color(200,88,51)); out of bound :(
+    bn::backdrop::set_color(bn::color(6, 18, 31));
     while (true)
     {
         bn::core::update();
