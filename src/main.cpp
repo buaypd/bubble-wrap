759445610d811d9552c93451cf89dfd8099b0bfd
Buay Dak <buaypd@gmail.com>
2026-01-14 18:34:42 +0000

    changed background color to skyblue

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_core.h>
-
+#include <bn_keypad.h>
 
 int main()
 {
     bn::core::init();
-    //bn::backdrop::set_color(bn::color(20, 20, 31)); // pld color
-    //bn::backdrop::set_color(bn::color(200,88,51)); out of bound :(
     bn::backdrop::set_color(bn::color(6, 18, 31));
     while (true)
     {
+        if (bn::keypad::a_pressed())
+        {
+            bn::backdrop::set_color(bn::color(31, 21, 22));
+        }
         bn::core::update();
     }
 }
