ffd6c81206d6f1033f8e50d82b85275c5bf139f6
Buay Dak <buaypd@gmail.com>
2026-01-20 07:23:01 +0000

    add a feat to when pressing down you get more dots

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,24 @@
 #include <bn_sprite_items_dot.h>
 #include <bn_log.h>
 #include <bn_vector.h>
+#include <bn_random.h>
 
 
 int main()
 {
 
     bn::core::init();
+    bn::random rng;
     bn::backdrop::set_color(bn::color(6, 18, 31));
     bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(17, 32);
-    bn::vector<bn::sprite_ptr, 20> circles = {};
+    bn::vector<bn::sprite_ptr, 1000> circles = {};
+    for (int y = -40; y <= 40; y += 15) {
     for(int x = -40; x <= 40; x += 15) {
-    bn::sprite_ptr myCircleLines = bn::sprite_items::dot.create_sprite(x, 40);
+    bn::sprite_ptr myCircleLines = bn::sprite_items::dot.create_sprite(x, y);
     circles.push_back(myCircleLines);
+    }
+     
+    
 }
 
     while (true)
@@ -34,6 +40,13 @@ int main()
         {
             bn::backdrop::set_color(bn::color(14, 8, 2));
         }
+        else if (bn::keypad::down_pressed()) {
+        int x = rng.get_int(-40,41);
+         int y= rng.get_int(-40,41);  
+        bn::sprite_ptr myRngCirle = bn::sprite_items::dot.create_sprite(x, y);
+        circles.push_back(myRngCirle);
+
+    }
 
         bn::core::update();
     }
